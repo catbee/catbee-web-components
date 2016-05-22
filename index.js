cfce53bebdbb3aee1abf18399fa4017e71a99413
Kirill Kaysarov <defcuts@mail.ru>
2016-05-22 20:23:08 +0600

    Better interface for document component registration

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ var DocumentRenderer = require('./lib/DocumentRenderer');
 module.exports = {
   register (locator, documentComponent) {
     locator.register('documentRenderer', DocumentRenderer, true);
-    locator.register('documentComponent', documentComponent);
+    locator.registerInstance('documentComponent', Object.assign(
+      {
+        name: 'document'
+      },
+      documentComponent
+    ));
   },
 
   DocumentRenderer
